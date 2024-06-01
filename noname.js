94a3a08ce2b8031931cca4295136b76d2889379c
gauravghimiree <ghimiregaurav773@gmail.com>
2024-06-01 22:29:42 +0545

    m

---
+++ noname.js
@@ -0,0 +1,9 @@
+//function with no name can also be defined in js which is defined below
+
+const plantNeedsWater = function(day){
+    if(day === 'Wednesday'){
+     return true;
+   }
+   else return false;
+ }
+ console.log(plantNeedsWater("Tuesday"));
