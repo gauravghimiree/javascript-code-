b44bf2c3a9edb3393632c745ddbdd5930ca60a89
gauravghimiree <ghimiregaurav773@gmail.com>
2024-06-01 22:14:45 +0545

    implementation of helper function in js

---
+++ helperFunction.js
@@ -0,0 +1,9 @@
+//helper funtion in js
+function monitorCount(rows, columns) {
+    return rows * columns;
+  }
+  function costOfMonitors (rows ,columns){
+   return  monitorCount (rows,columns) *200
+  }
+  const totalCost =  costOfMonitors(5,4);
+  console.log(totalCost);
