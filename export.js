f928cda2fc7e5ac4171c5fadbc7fe2081c1c5964
gauravghimiree <ghimiregaurav773@gmail.com>
2024-06-12 23:29:03 +0545

    this is the simple example of import and export using es6 modul

---
+++ export.js
@@ -0,0 +1,13 @@
+const name = (name) => {
+    console.log(`hello my name is ${name}`);
+}
+
+const age = (age) => {
+    console.log(`I am ${age} years old`);
+}
+
+const address = (address) => {
+    console.log(`I am from ${address}`);
+}
+
+export { name, age, address };
---
+++ import.js
@@ -0,0 +1,2 @@
+import { name, age , address } from  "./export.js"
+console.log(name('Gaurav'));
